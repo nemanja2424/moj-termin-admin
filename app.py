1cb33c810aec2e7ff890110ea69c0a9770a1aa85
nemanja2424 <njakovlje@gmail.com>
2026-03-16 22:27:09 +0100

    token

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager, create_access_token
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET")  # nemanjica
+jwt = JWTManager(app)
+
+# ruta za login koja vraća token koji ne ističe
+@app.route('/token', methods=['POST'])
+def get_token():
+    data = request.json
+    # proverava korisničko ime i lozinku iz .env
+    if data.get("username") == os.getenv("USERNAME") and data.get("password") == os.getenv("PWD"):
+        # expires_delta=None znači da token NE ISTIČE
+        token = create_access_token(identity=data["username"], expires_delta=None)
+        return jsonify(access_token=token), 200
+    return jsonify(msg="Bad credentials"), 401
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
